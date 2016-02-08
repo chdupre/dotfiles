6959d9b6e73fb9c8632fd5ec61d1aebbbbd6ee77
Christophe Dupre <ch_dupre@yahoo.com>
2016-02-08 19:57:21 +0000

    changed font for i3 bashrc added

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -9,7 +9,7 @@
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 #files="bashrc vimrc vim zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
-files="vimrc vim i3 tmux.conf i3status.conf"    # list of files/folders to symlink in homedir
+files="vimrc vim i3 tmux.conf bashrc i3status.conf"    # list of files/folders to symlink in homedir
 
 ##########
 
